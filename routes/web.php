4dd1f667d69d24c687e457272b36dc30a7dedecb
Dauda Pona <daudapona2@gmail.com>
2024-07-15 13:00:59 +0100

    fix: redirect root page to applicant login

--- routes/web.php
+++ routes/web.php
@@ -2,5 +2,9 @@
 
 use Illuminate\Support\Facades\Route;
 
+Route::get('/', function() {
+    return redirect()->route('applicant.authentication.login.display-login-form');
+});
+
 Route::prefix('/applicant')->group(__DIR__ . '/Web/applicant.php');
 Route::prefix('/admin')->group(__DIR__.'/Web/admin.php');
