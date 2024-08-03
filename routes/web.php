ace00c02e3ff5cce34ba42d0c8d1e537534f39d0
Dauda Pona <daudapona2@gmail.com>
2024-08-03 09:34:42 +0100

    fix: fixed the scholarship board

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,6 @@
 
 use Illuminate\Support\Facades\Route;
 
-Route::prefix('/applicant')->group(__DIR__ . '/Web/applicant.php');
+// Route::prefix('/applicant')->group(__DIR__ . '/Web/applicant.php');
 Route::prefix('/admin')->group(__DIR__.'/Web/admin.php');
 Route::prefix('/')->group(__DIR__.'/Web/public.php');
